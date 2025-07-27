5a8614d6d7585a8608100cf76bf2abf48bba2205
HuyDolphin <huyb2308360@student.ctu.edu.vn>
2025-07-27 18:23:02 +0700

    Updated file and folder Updated gameList

--- script/home.js
+++ script/home.js
@@ -1,3 +1,28 @@
 function redirect(link){
     window.location.href = "gameHTML/" + link + ".html";
 }
+function showHome(){
+    const container = document.querySelector(".AG_container");
+        Object.keys(gameList).forEach(key => {
+            const game = gameList[key];
+            if (true) {
+                container.innerHTML += `                
+                <div class="GameCard" onclick="redirect('${game.dataName}')">
+                    <div class="image-container">
+                        <img src="${game.coverImage}" alt="gameCover">
+                    </div>
+                    <div class="content">
+                        <h1>${game.displayName}</h1>
+                        <div class="priceGroup">
+                            <h6 class="saleAmount">-${game.Sale * 100}%</h6>
+                            <div class="price">
+                                <p>${game.originalPrice.toLocaleString("vi-VN", {style: "currency", currency: "VND"})}</p>
+                                <p class="sale">${game.getPrice().toLocaleString("vi-VN", {style: "currency", currency: "VND"})}</p>
+                            </div>  
+                        </div>
+                    </div>
+                </div>`;
+            }
+    })
+}
+showHome();
